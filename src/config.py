9b9e0ca4ffb8f2915607cc219376f2215a1b2bb4
Vitalii Honchar <honchar.v@ajax.systems>
2021-11-27 15:39:27 +0200

    Added PostgreSQL

--- src/config.py
+++ src/config.py
@@ -1,8 +1,16 @@
 import logging, coloredlogs, sys, yaml, os
 
-PARAM_TOKEN = 'token'
-PARAM_TIMEOUT_SECONDS = 'timeout-seconds'
-PARAM_STORAGE = 'storage'
+PARAM_BOT = 'bot'
+PARAM_BOT_TOKEN = 'token'
+PARAM_BOT_TIMEOUT_SECONDS = 'timeout-seconds'
+
+PARAM_DATABASE = 'database'
+PARAM_DATABASE_HOST = 'host'
+PARAM_DATABASE_NAME = 'name'
+PARAM_DATABASE_USER = 'user'
+PARAM_DATABASE_PASSWORD = 'password'
+PARAM_DATABASE_PORT = 'port'
+
 CONFIG_FILE = 'hacker-bot.yaml'
 
 def _init_logging():
@@ -18,32 +26,55 @@ def _create_config():
 
     with open(config_file) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-        bot = config['bot']
-        return BotConfig(
-            token=_get_param(bot, PARAM_TOKEN), 
-            timeout_seconds=int(_get_param(bot, PARAM_TIMEOUT_SECONDS)), 
-            storage=_get_param(bot, PARAM_STORAGE)
-        )
+        bot = config[PARAM_BOT]
+        database = config[PARAM_DATABASE]
+        return (_create_bot_config(bot), _create_database_config(database))
 
-def _get_param(bot_config, param):
-    env_param = f"BOT_{param.replace('-', '_').upper()}"
+def _create_bot_config(bot_config):
+    return BotConfig(
+        token=_get_param(bot_config, PARAM_BOT_TOKEN, PARAM_BOT), 
+        timeout_seconds=int(_get_param(bot_config, PARAM_BOT_TIMEOUT_SECONDS, PARAM_BOT)), 
+    )
+
+def _create_database_config(db_config):
+    return DatabaseConfig(
+        host=_get_param(db_config, PARAM_DATABASE_HOST, PARAM_DATABASE),
+        name=_get_param(db_config, PARAM_DATABASE_NAME, PARAM_DATABASE),
+        user=_get_param(db_config, PARAM_DATABASE_USER, PARAM_DATABASE),
+        password=_get_param(db_config, PARAM_DATABASE_PASSWORD, PARAM_DATABASE),
+        port=int(_get_param(db_config, PARAM_DATABASE_PORT, PARAM_DATABASE))
+    )
+
+def _get_param(config, param, prefix):
+    env_param = f"{prefix}_{param.replace('-', '_').upper()}"
     value = os.environ.get(env_param)
     if value is not None:
         return value
-    return bot_config[param]
+    return config[param]
 
 def init():
-    global bot_config
+    global bot_config, db_config
     _init_logging()
-    bot_config = _create_config()
+    bot_config, db_config = _create_config()
 
 class BotConfig:
-    def __init__(self, token: str, timeout_seconds: int, storage: str):
+    def __init__(self, token: str, timeout_seconds: int):
         self.token = token
         self.timeout_seconds = timeout_seconds
-        self.storage = storage
     
     def __repr__(self) -> str:
-        return f"BotConfig(token={self.token[:3]}***{self.token[len(self.token) - 3:]}, timeout_seconds={self.timeout_seconds}, storage={self.storage})"
-    
+        return f"BotConfig(token={self.token[:3]}***{self.token[len(self.token) - 3:]}, timeout_seconds={self.timeout_seconds})"
+
+class DatabaseConfig:
+    def __init__(self, host: str, name: str, user: str, password: str, port: int):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.port = port
+
+    def __repr__(self) -> str:
+        return f"DatabaseConfig(host={self.host}, name={self.name}, user={self.user}, password=***, port={self.port})"
+
 bot_config: BotConfig = None
+db_config: DatabaseConfig = None
--- src/storage.py
+++ src/storage.py
@@ -1,4 +1,4 @@
-import config, sqlite3
+import config, psycopg2
 from contextlib import contextmanager
 from typing import List, Any
 from domain import Subscriber
@@ -16,17 +16,18 @@ SQL_INSERT_SUBSCRIBER = '''
     INSERT INTO subscriber 
         (id, first_name, last_name, chat_id)
     VALUES 
-        (:id, :first_name, :last_name, :chat_id)
+        (%(id)s, %(first_name)s, %(last_name)s, %(chat_id)s)
 '''
 
-SQL_READ_SUBSCRIBER = 'SELECT * FROM subscriber WHERE id = :id'
+SQL_READ_SUBSCRIBER = 'SELECT * FROM subscriber WHERE id = %(id)s'
 
 SQL_READ_SUBSCRIBERS = 'SELECT * FROM subscriber'
 
 
 def get_subscribers() -> list[Subscriber]:
     with _read_cursor() as cursor:
-        rows = cursor.execute(SQL_READ_SUBSCRIBERS).fetchall()
+        cursor.execute(SQL_READ_SUBSCRIBERS)
+        rows = cursor.fetchall()
         subscribers = []
         for row in rows:
             subscribers.append(_map_row_to_subscriber(row))
@@ -45,7 +46,8 @@ def init():
         cursor.execute(SQL_CREATE_SCHEMA)
 
 def _is_subscriber_exists(subscriber, cursor):
-    return len(cursor.execute(SQL_READ_SUBSCRIBER, {'id': subscriber.id}).fetchall()) > 0
+    cursor.execute(SQL_READ_SUBSCRIBER, {'id': subscriber.id})
+    return len(cursor.fetchall()) > 0
 
 def _map_row_to_subscriber(row) -> Subscriber:
     return Subscriber(id=row[0], first_name=row[1], last_name=row[2], chat_id=row[3])
@@ -66,14 +68,22 @@ def _read_cursor():
 @contextmanager
 def _update_cusor():
     with _connection() as conn:
+        cursor = conn.cursor()
         try:
-            yield conn.cursor()
+            yield cursor
         finally:
             conn.commit() 
+            cursor.close()
 
 @contextmanager
 def _connection():
-    connection = sqlite3.connect(config.bot_config.storage)
+    connection = psycopg2.connect(
+        host=config.db_config.host,
+        database=config.db_config.name,
+        user=config.db_config.user,
+        password=config.db_config.password,
+        port=config.db_config.port
+    )
     try:
         yield connection
     finally:
