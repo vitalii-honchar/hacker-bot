91860e07c8601ed54b8de01c007db95ce3c47605
Vitalii Honchar <honchar.v@ajax.systems>
2021-12-13 08:17:09 +0200

    Fixed domain

--- src/domain.py
+++ src/domain.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass, replace
 from datetime import datetime, timedelta
 
+def _next_notification_time():
+    return (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0, second=0, microsecond=0)
+
 @dataclass(frozen=True)
 class Subscriber:
-    NOTIFICATION_TIMEOUT = timedelta(days=1)
-
     id: str
     chat_id: str
     first_name: str
     last_name: str
-    notification_time: datetime = (datetime.utcnow() + NOTIFICATION_TIMEOUT).replace(hour=6, minute=0, second=0, microsecond=0)
+    notification_time: datetime = _next_notification_time()
 
     def update_notification_time(self):
-        return replace(self, notification_time=self.notification_time + Subscriber.NOTIFICATION_TIMEOUT)
+        return replace(self, notification_time=_next_notification_time())
 
 @dataclass(frozen=True)
 class Article:
