456f647ea6cc0cad01e56f31cc9ff988a11f37a1
Vitalii Honchar <honchar.v@ajax.systems>
2021-12-16 08:32:12 +0200

    Added try and catch for send message to subscriber

--- src/news_sender.py
+++ src/news_sender.py
@@ -1,20 +1,24 @@
 import asyncio, hackernews, logging, messages
 from storage.subscriber.subscriber import get_subscribers_for_notifications, save_subscribers
 from aiogram import Bot
-
+e
 async def _send_news(bot: Bot):
     subscribers = await get_subscribers_for_notifications()
     if len(subscribers) > 0:
         subsribers_for_save = []
         articles = await hackernews.get_top_arcticles()
         for subscriber in subscribers:
-            logging.info('Send articles to subscriber: subscriber = {}'.format(subscriber))
-            await bot.send_message(
-                chat_id=subscriber.chat_id, 
-                text=messages.create_message(articles),
-                parse_mode=messages.PARSE_MODE_MARKDOWN
-            )
-            subsribers_for_save.append(subscriber.update_notification_time())
+            msg = messages.create_message(articles)
+            try:
+                logging.info('Send articles to subscriber: subscriber = {}, message = {}'.format(subscriber, msg))
+                await bot.send_message(
+                    chat_id=subscriber.chat_id, 
+                    text=msg,
+                    parse_mode=messages.PARSE_MODE_MARKDOWN
+                )
+                subsribers_for_save.append(subscriber.update_notification_time())
+            except Exception as e:
+                logging.error('Error during send article: subscriber = {}, message = {}'.format(subscriber, msg))
         await save_subscribers(subsribers_for_save)
 
 async def _start_send_news(period: int, bot: Bot):
